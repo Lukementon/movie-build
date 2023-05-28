0ff0c47f533ffc736c8a65e193080d0dccaf8692
Lukementon <lukementon15@gmail.com>
2023-05-28 17:57:21 +0100

    progress

--- src/components/row/Row.tsx
+++ src/components/row/Row.tsx
@@ -7,32 +7,31 @@ export interface RowProps {
   title?: string;
 }
 
-const Row = ({ movies, title }: RowProps) => {
-  return (
-    <div className='mt-8'>
-      <h4 className='text-3xl text-white'>{title}</h4>
-      <div className='flex items-center mt-4'>
-        <div className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
-          {movies?.results.map(movie => (
-            <div
-              key={movie.id}
-              className='h-56 w-96 relative mr-8 rounded-2xl overflow-hidden cursor-pointer inline-block'
-            >
-              <Image
-                src={`${config.MOVIE_DB_IMAGE_BASE_URL}${movie.backdrop_path}`}
-                alt={movie.original_title}
-                fill={true}
-                sizes='100%'
-                style={{
-                  objectFit: 'contain',
-                }}
-              />
-            </div>
-          ))}
-        </div>
+const Row = ({ movies, title }: RowProps) => (
+  <div className='mt-8'>
+    <h4 className='text-3xl text-white'>{title}</h4>
+    <div className='flex items-center mt-4'>
+      <div className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+        {movies?.results.map(movie => (
+          <div
+            key={movie.id}
+            className='h-56 w-96 relative mr-8 rounded-2xl overflow-hidden cursor-pointer inline-block'
+          >
+            <Image
+              src={`${config.MOVIE_DB_IMAGE_BASE_URL}${movie.backdrop_path}`}
+              alt={movie.original_title}
+              priority={false}
+              fill={true}
+              sizes='100%'
+              style={{
+                objectFit: 'contain',
+              }}
+            />
+          </div>
+        ))}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Row;
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   MOVIE_DB_BASE_URL: 'https://api.themoviedb.org/3',
-  MOVIE_DB_IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/original/',
+  MOVIE_DB_IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/original',
   REACT_QUERY_BASE_URL: 'http://localhost:3000/api/graphql',
 } as const;
 
